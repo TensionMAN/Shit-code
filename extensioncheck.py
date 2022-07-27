949efce3531e0b8e2f72cc8fba3eb8f1dddaacd1
TensionMAN <109971121+TensionMAN@users.noreply.github.com>
2022-07-27 18:17:29 +0300

    Add files via upload

--- extensioncheck.py
+++ extensioncheck.py
@@ -2,6 +2,7 @@ import imghdr
 import os
 import os.path
 import time
+import shutil
 
 start_time = time.time()
 checkdir = r"D:\user\Designers2Managers\эскизы"
@@ -16,17 +17,19 @@ for address, dirs, files in files_tree:
 	for file in files:
 		file_counter += 1
 		full_path = os.path.join(address, file).replace("\\","/") #вычисляем полный путь для передачи в imghdr
-		file_directory = os.path.join(address) + "/"
+		file_directory = os.path.join(address)
 		extension = imghdr.what(full_path) #узнаем формат изображения
 		if extension != "jpeg": #Проверка условия что картинка не является jpegом
                         if file == "Thumbs.db" or file == "sync.ffs_db": #исключение из логирования системных файлов
                                 #print(file + " системный файл") #debug
-                                os.remove(file_directory + file) #все равно удаляем, во измежание попыток мимикрирования под системный файл
+                                os.remove(os.path.join(file_directory, file)) #все равно удаляем, во измежание попыток мимикрирования под системный файл
+                                #реализовать проверку sync.ffs_db по основному пути и не трогать его
                         else:
                                 deleted_file_counter += 1
                                 print(file + " WARNING, FILE WILL BE DELETED")
-                                os.replace(file_directory.replace("\\","/") + file, deleted_files_dir.replace("\\","/") + file) #Переносим файл в карантин для аудита
+                                shutil.move(os.path.join(file_directory, file), os.path.join(deleted_files_dir, file)) #Переносим файл в карантин для аудита
                                 log_deleted_file.write(full_path + '\n') #пишем в лог полный путь и имя файла
+                                #реализовать алерт в телеграм
 		else:
                         pass
                         #print(file + " FILE IS JPEG") #не трогаем
