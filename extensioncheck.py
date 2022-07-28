e7c14c608e5c7a0980279b9df4a08769f912dbf9
TensionMAN <109971121+TensionMAN@users.noreply.github.com>
2022-07-28 11:49:46 +0300

    Add files via upload

--- extensioncheck.py
+++ extensioncheck.py
@@ -3,25 +3,27 @@ import os
 import os.path
 import time
 import shutil
+import requests
 
 start_time = time.time()
-checkdir = r"D:\user\Designers2Managers\эскизы"
-deleted_files_dir = r"C:\Users\sadmin\Desktop\d2m_check\deleted_files"
+checkdir = r"D:\YOUR_PATH"
+deleted_files_dir = r"C:\YOUR_PATH"
 files_tree = os.walk(checkdir)
 file_counter = 0
 deleted_file_counter = 0
 log_deleted_file = open("deleted.txt", "a") #открываем запись в лог
+tg_url=r"https://api.telegram.org/bot[KEY]/sendMessage?chat_id=[CHAT_ID]&text=ACHTUNG!"
 
 #Перебираем директорию
 for address, dirs, files in files_tree:
-	for file in files:
-		file_counter += 1
-		full_path = os.path.join(address, file).replace("\\","/") #вычисляем полный путь для передачи в imghdr
-		file_directory = os.path.join(address)
-		extension = imghdr.what(full_path) #узнаем формат изображения
-		if extension != "jpeg": #Проверка условия что картинка не является jpegом
+        for file in files:
+                file_counter += 1
+                full_path = os.path.join(address, file).replace("\\","/") #вычисляем полный путь для передачи в imghdr
+                file_directory = os.path.join(address)
+                extension = imghdr.what(full_path) #узнаем формат изображения
+                if extension != "jpeg": #Проверка условия что картинка не является jpegом
                         if file == "Thumbs.db" or file == "sync.ffs_db": #исключение из логирования системных файлов
-                                        if full_path != "D:/user/Designers2Managers/эскизы/sync.ffs_db": #доп проверка на корневой файл синхронизации
+                                        if full_path != "D:/YOUR_ROOT_MIRROR_SYNC_FOLDER_PATH/sync.ffs_db": #доп проверка на корневой файл синхронизации
                                                 os.remove(os.path.join(file_directory, file)) #все равно удаляем, во измежание попыток мимикрирования под системный файл
                                         else:
                                                 print(full_path + " файл синхронизации sync, не трогаем")
@@ -30,8 +32,8 @@ for address, dirs, files in files_tree:
                                 print(file + " WARNING, FILE WILL BE DELETED")
                                 shutil.move(os.path.join(file_directory, file), os.path.join(deleted_files_dir, file)) #Переносим файл в карантин для аудита
                                 log_deleted_file.write(full_path + '\n') #пишем в лог полный путь и имя файла
-                                #реализовать алерт в телеграм
-		else:
+                                requests.post(tg_url)
+                else:
                         pass
                         #print(file + " FILE IS JPEG") #не трогаем
 
