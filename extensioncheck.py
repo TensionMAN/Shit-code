74739c96d4806ee204a9d24c93e8fc6625099ada
TensionMAN <109971121+TensionMAN@users.noreply.github.com>
2022-07-28 11:22:00 +0300

    Add files via upload

--- extensioncheck.py
+++ extensioncheck.py
@@ -10,7 +10,7 @@ deleted_files_dir = r"C:\Users\sadmin\Desktop\d2m_check\deleted_files"
 files_tree = os.walk(checkdir)
 file_counter = 0
 deleted_file_counter = 0
-log_deleted_file = open("deleted.txt", "a")
+log_deleted_file = open("deleted.txt", "a") #открываем запись в лог
 
 #Перебираем директорию
 for address, dirs, files in files_tree:
@@ -21,9 +21,10 @@ for address, dirs, files in files_tree:
 		extension = imghdr.what(full_path) #узнаем формат изображения
 		if extension != "jpeg": #Проверка условия что картинка не является jpegом
                         if file == "Thumbs.db" or file == "sync.ffs_db": #исключение из логирования системных файлов
-                                #print(file + " системный файл") #debug
-                                os.remove(os.path.join(file_directory, file)) #все равно удаляем, во измежание попыток мимикрирования под системный файл
-                                #реализовать проверку sync.ffs_db по основному пути и не трогать его
+                                        if full_path != "D:/user/Designers2Managers/эскизы/sync.ffs_db": #доп проверка на корневой файл синхронизации
+                                                os.remove(os.path.join(file_directory, file)) #все равно удаляем, во измежание попыток мимикрирования под системный файл
+                                        else:
+                                                print(full_path + " файл синхронизации sync, не трогаем")
                         else:
                                 deleted_file_counter += 1
                                 print(file + " WARNING, FILE WILL BE DELETED")
